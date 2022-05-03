0e01aa83de152c8a8efcce67d672884637a2a523
Vishnu Srinivasrao Biradar <vishnu.biradar@yash.com>
2022-05-03 14:14:58 +0530

    Added some code

--- marketing/config/webpack.common.js
+++ marketing/config/webpack.common.js
@@ -1,20 +1,3 @@
-// module.exports= {
-//     rules:[
-//         {
-//             test: /\.m?js$/ ,
-//             exclude: /node_modules/,
-//             use:{
-//                 loader: 'babel-loader',
-//                 option:{
-//                     presets:['@babel/preset-react','@babel/preset-env'],
-//                     Plugins :['@babel/plugin-transform-runtime'],
-//                 },
-//             },
-
-//         },
-//     ],
-// };
-
 module.exports = {
     module: {
       rules: [
